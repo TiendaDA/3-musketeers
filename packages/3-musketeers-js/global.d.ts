27d8096e4da8e8c1e0f414dc0f7896a0020cf212
Carlos Leonardo Cruces Huaman <carlosgamercovi20@gmail.com>
2023-08-29 14:09:57 -0500

    add types to global ts

--- packages/3-musketeers-js/global.d.ts
+++ packages/3-musketeers-js/global.d.ts
@@ -2,6 +2,30 @@ interface Window {
   dataQueue: unknown[];
   dataLayer: unknown[];
   gtag: (...args: unknown[]) => void;
-  amplitude;
   t3musketeers: unknown;
+  _fbq: unknown;
+  fbq: {
+    callMethod: (...args: unknown[]) => void;
+    push: (...args: unknown[]) => void;
+    loaded: boolean;
+    version: string;
+    queue: unknown[];
+  };
+  _cio: {
+    push: (...args: unknown[]) => void;
+    page: (...args: unknown[]) => void;
+    track: (...args: unknown[]) => void;
+    identify: (...args: unknown[]) => void;
+  };
+  userGuidingLayer: unknown[];
+  userGuiding: {
+    previewGuide: (...args: unknown[]) => void;
+    finishPreview: (...args: unknown[]) => void;
+    track: (...args: unknown[]) => void;
+    identify: (...args: unknown[]) => void;
+    hideChecklist: (...args: unknown[]) => void;
+    launchChecklist: (...args: unknown[]) => void;
+    q: unknown[];
+    c: (...args: unknown[]) => void;
+  };
 }
