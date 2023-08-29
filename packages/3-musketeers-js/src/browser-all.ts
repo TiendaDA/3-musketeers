f6b87070708869792ad3981589f25a1711629e78
Carlos Leonardo Cruces Huaman <carlosgamercovi20@gmail.com>
2023-08-29 14:11:09 -0500

    update all providers

--- packages/3-musketeers-js/src/browser-all.ts
+++ packages/3-musketeers-js/src/browser-all.ts
@@ -1,7 +1,18 @@
 import {Tiendada3musketeers} from './3-musketeers';
 import {GoogleAnalytics} from './3-musketeers/provider/google-analytics';
 import {Amplitude} from './3-musketeers/provider/amplitude';
+import {CustomerIo} from './3-musketeers/provider/customer-io';
+import {FacebookPixel} from './3-musketeers/provider/facebook-pixel';
+import {Hotjar} from './3-musketeers/provider/hotjar';
+import {UserGuiding} from './3-musketeers/provider/user-guiding';
 
-const t3musketeers = new Tiendada3musketeers([GoogleAnalytics, Amplitude]);
+const t3musketeers = new Tiendada3musketeers([
+  GoogleAnalytics,
+  Amplitude,
+  CustomerIo,
+  FacebookPixel,
+  Hotjar,
+  UserGuiding,
+]);
 
 window.t3musketeers = t3musketeers;
